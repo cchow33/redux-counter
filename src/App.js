fbad78e51ab988c10d734dece7445b99d160aaf5
Carmen Chow <carmendevelops@gmail.com>
2022-12-03 18:25:47 -0500

    add button

--- src/App.js
+++ src/App.js
@@ -2,55 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {connect} from 'react-redux';
 
-function App() {
+// Connect
+// connect ('What parts of state do you want?', 'What actions do you want to dispatch?') (Component)
+
+// connect (mapStateToPropsFunc, mapDispatchToPropsFunc) (Component)
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <h1>Count Goes Here</h1>
+      <button>-</button>
+      <button>+</button>
     </div>
   );
 }
